a5fcdabe5830b126d67eb5254014f710b8f1186d
Charles <charleslanop@gmail.com>
2021-01-22 22:02:36 -0300

    Blog updated v1

---
+++ src/modules/users/interfaces/PaginateUserInterface.ts
@@ -0,0 +1,12 @@
+import User from "../typeorm/entities/User";
+
+export default interface PaginateUserInterface {
+    from: number;
+    to: number;
+    per_page: number;
+    total: number;
+    current_page: number;
+    prev_page: number | null;
+    next_page: number | null;
+    data: User[];
+}
--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -1,14 +1,17 @@
 import User from "../typeorm/entities/User";
 import {getCustomRepository} from "typeorm";
 import UsersRepository from "../typeorm/repositories/UsersRepository";
+import PaginateUserInterface from "../interfaces/PaginateUserInterface";
 
 class ListUserService {
 
-    public async execute(): Promise<User[]> {
+    public async execute(): Promise<PaginateUserInterface> {
 
         const userRepository = getCustomRepository(UsersRepository);
 
-        return await userRepository.find();
+        const users = await userRepository.createQueryBuilder().paginate();
+
+        return users as PaginateUserInterface;
     }
 }
 
