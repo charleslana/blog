e58d27741568b5c4349ab4331664699692513852
Charles <charleslanop@gmail.com>
2021-01-24 16:53:34 -0300

    Blog updated v1

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -45,8 +45,7 @@ app.use((error: Error, request: Request, response: Response, next: NextFunction)
     return response.status(500).json({
         statusCode: 500,
         status: 'error',
-        // message: 'Internal server error'
-        message: error.message
+        message: 'Internal server error'
     });
 });
 
