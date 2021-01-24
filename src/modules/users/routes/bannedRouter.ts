c667968ebcda77d908ed0026ce3dbc4e5399f783
Charles <charleslanop@gmail.com>
2021-01-24 02:15:48 -0300

    Blog updated v1

--- src/modules/users/routes/bannedRouter.ts
+++ src/modules/users/routes/bannedRouter.ts
@@ -2,14 +2,13 @@ import {Router} from "express";
 import authenticatedUser from "../../../shared/http/middlewares/authenticatedUser";
 import {celebrate, Joi, Segments} from "celebrate";
 import BannedController from "../controllers/BannedController";
-import roleRouter from "./roleRouter";
 import UsersBannedEnum from "../enumerations/UsersBannedEnum";
 import restrictedAccessForRoleOnlyAdmin from "../../../shared/http/middlewares/restrictedAccessForRoleOnlyAdmin";
 
 const bannedRouter = Router();
 const bannedController = new BannedController();
 
-roleRouter.put('/', authenticatedUser, restrictedAccessForRoleOnlyAdmin, celebrate({
+bannedRouter.put('/', authenticatedUser, restrictedAccessForRoleOnlyAdmin, celebrate({
     [Segments.BODY]: {
         id: Joi.number().required(),
         banned: Joi.string().valid(...Object.values(UsersBannedEnum)).required()
